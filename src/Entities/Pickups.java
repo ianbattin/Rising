8dc2a7b8031a6529118490cdd3eb364eddccf576
maxwey <maxence.weyrich@gmail.com>
2015-10-20 00:36:43 -0700

    Changed Pickups
    -Changed how location of pickup is calculated (now uses a sine function)
    -Moved the code out of the update method and into a separate method

--- src/Entities/Pickups.java
+++ src/Entities/Pickups.java
@@ -10,8 +10,7 @@ public class Pickups {
 	
 	private long coolDownTime;
 	private boolean willDrawPickup;
-	private float xLoc, yLoc;
-	private float xVelocity;
+	private float xLoc, yLoc, offSet;
 	
 	public Pickups(Player player)
 	{
@@ -19,42 +18,43 @@ public class Pickups {
 		
 		coolDownTime = 10000000000L;
 		willDrawPickup = false;
-		xLoc = yLoc = 0;
-		xVelocity = 0;
+		xLoc = GamePanel.WIDTH/2;
+		yLoc = -50;
+		offSet = (float)(Math.random()*GamePanel.HEIGHT);
+		System.out.println(offSet);
 	}
 
 	//updates by checking to see if it should spawn a pickup.
-	//TODO: move code out of update and into methods
 	public void update()
 	{	
 		if	(!willDrawPickup)
 		{
-			if (coolDownTime > 0)
-			{
-				coolDownTime -= GamePanel.getElapsedTime();
-			} 
-			else 
-			{
-				if ((int)(Math.random()*0) == 0)//edit probability of spawning here
-				{
-					coolDownTime = 100000000000L;
-					xLoc = (int)(Math.random()*(GamePanel.WIDTH-150))+75;
-					if (((int)(Math.random()*3)-1) > 0)
-						xVelocity = 1;
-					else 
-						xVelocity = -1;
-						
-					willDrawPickup = true;
-				}
-				else
-				{
-					coolDownTime = 10000000000L;
-				}
-			}
-		} 
+			checkPickups();
+		}
 		else
 		{
 			checkCollision();
+		}
+	}
+	
+	//check to see if should spawn a pickup
+	public void checkPickups()
+	{
+		if (coolDownTime > 0)
+		{
+			coolDownTime -= GamePanel.getElapsedTime();
+		} 
+		else 
+		{
+			if ((int)(Math.random()*0) == 0)//edit probability of spawning here
+			{
+				coolDownTime = 100000000000L;
+				willDrawPickup = true;
+			}
+			else
+			{
+				coolDownTime = 10000000000L;
+			}
 		}
 	}
 	
@@ -70,28 +70,14 @@ public class Pickups {
 				yLoc += 0.5;
 			else 
 				willDrawPickup = false;
-			xLoc += xVelocity;
-			
-			if((xLoc+100) > GamePanel.WIDTH && xVelocity > -1)
-			{
-				xVelocity -= 0.0075;
-			} 
-			else if(xLoc < 75 && xVelocity < 1)
-			{
-				xVelocity += 0.0075;
-			}
-			else if (xVelocity != 1 || xVelocity != -1)
-			{
-				xVelocity = xVelocity/Math.abs(xVelocity);
-			}
+			xLoc = (float)(Math.sin((yLoc-offSet)/(GamePanel.HEIGHT/4))*(GamePanel.WIDTH/2-50))+GamePanel.WIDTH/2;
 		}
 	}
 	
 	//checks if the player collided with the pickup
 	public void checkCollision()
 	{
-		if ((player.getX()-player.getWidth()/2) < xLoc+5 && xLoc-5 < (player.getX()+player.getWidth()/2) && (player.getY()-player.getHeight()/2) < yLoc-5 && yLoc+5 < (player.getY()+player.getHeight()/2))
+		if ((player.getX()-(player.getWidth()/2)) < xLoc+10 && xLoc-10 < (player.getX()+(player.getWidth()/2)) && (player.getY()-player.getHeight()/2) < yLoc+10 && yLoc-10 < (player.getY()+player.getHeight()/2))
 			willDrawPickup = false;
-			
 	}
 }
