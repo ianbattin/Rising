0cc039656299c5723c9af8107ff562b2db0ec79e
ianbattin <ianbattin0@gmail.com>
2015-11-04 17:08:22 -0800

    Changed music. Need to figure out volume control
    Music is now modero. It sounds pretty quiet so well have to figure out
    volume

--- src/GameState/SplashState.java
+++ src/GameState/SplashState.java
@@ -28,7 +28,7 @@ public class SplashState extends GameState
 	
 	public void init() 
 	{
-		music("Resources/Sound/Cybernator_-_Fully_Set_Up_For_Penetration.wav");
+		music("Resources/Sound/Modero.wav");
 	}
 
 
